ba1e79a49799b73c9c6e4ffedad62bdd216511e2
Florian Bomers <Florian.Bomers@bome.com>
2022-03-27 16:50:42 +0200

    Export .ico and .png files in various sizes to same folder as main .svg image file. - export different variations useful for macOS and iOS

--- CreateIconFile.py
+++ CreateIconFile.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 
 # Copyright 2022 Sven Schork
+# Copyright 2022 Florian Bomers
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy of 
 # this software and associated documentation files (the "CreateIconFile"), to deal in the 
@@ -25,70 +26,98 @@ from pathlib import Path
 from PIL import Image
 from inkex.command import inkscape
 from inkex.base import TempDirMixin
+import os
 
 class CreateIconFile (TempDirMixin, inkex.Effect):
 
     def __init__(self):
         inkex.Effect.__init__(self)
 
-        self.arg_parser.add_argument('-t', '--iconfilename',
-            type = str, dest = 'iconfilename', default = 'newicon.ico',
-            help = 'The selected icon filename with path')
+        self.arg_parser.add_argument('-t', '--iconname',
+            type = str, dest = 'iconname', default = 'icon.ico',
+            help = 'The selected icon name without path (will be created at same location as .svg)')
 
-    def createIconFile(self, exportpar):
+    def createIconFile(self, exportpar, icofilename):
         # Write the global header
-        number_of_sources = len(exportpar)
+        number_of_sources = 0
+        for exportfilepar in exportpar:
+            if (exportfilepar['ico']):
+                number_of_sources = number_of_sources + 1
         data = bytes((0, 0, 1, 0, number_of_sources, 0))
         offset = 6 + number_of_sources * 16
 
         # Write the header entries for each individual image
         for exportfilepar in exportpar:
-            img = Image.open(exportfilepar['filename'])
-            # inkex.errormsg("Width: " + str(img.width) + ", Height: " + str(img.height))
-            if (img.width >= 256): # Icon max size 256 needs to set the size bytes to zero
-                data += bytes((0, 0, 0, 0, 1, 0, 32, 0, ))
-            else:
-                data += bytes((img.width, img.height, 0, 0, 1, 0, 32, 0, ))
-            bytesize = Path(exportfilepar['filename']).stat().st_size
-            data += bytesize.to_bytes(4, byteorder="little")
-            data += offset.to_bytes(4, byteorder="little")
-            offset += bytesize
+            if (exportfilepar['ico']):
+                img = Image.open(exportfilepar['filename'])
+                # inkex.errormsg("Width: " + str(img.width) + ", Height: " + str(img.height))
+                if (img.width >= 256): # Icon max size 256 needs to set the size bytes to zero
+                    data += bytes((0, 0, 0, 0, 1, 0, 32, 0, ))
+                else:
+                    data += bytes((img.width, img.height, 0, 0, 1, 0, 32, 0, ))
+                bytesize = Path(exportfilepar['filename']).stat().st_size
+                data += bytesize.to_bytes(4, byteorder="little")
+                data += offset.to_bytes(4, byteorder="little")
+                offset += bytesize
+                img.close()
 
         # Write the image data to the icon file
         for exportfilepar in exportpar:
-            data += Path(exportfilepar['filename']).read_bytes()
+            if (exportfilepar['ico']):
+                data += Path(exportfilepar['filename']).read_bytes()
         # Save the icon file
-        Path(self.options.iconfilename).write_bytes(data)
+        Path(icofilename).write_bytes(data)
 
         return data
 
     def effect(self):
         try:
             svg = self.svg
-            # inkex.utils.debug("start create icon: " + self.options.iconfilename)
-            
+            #inkex.utils.debug("start create icon: " + self.options.iconname)
+            basename = os.path.splitext(self.options.iconname)[0]
+            # prepend current file path if icon path is not absolute
+            if basename[0] != '/' and basename[1] != ':':
+                basename = os.path.join(self.svg_path(), basename)
+                    
             # inkex.utils.debug("TempDir: " + self.tempdir)
+            #inkex.utils.debug("svg.name: " + self.svg_path())
+            #inkex.utils.debug("basename: " + basename)
             export = [
-                {'filename': self.tempdir + "/" + "image_016.png", 'size': 16, 'dpi': 96},
-                {'filename': self.tempdir + "/" + "image_024.png", 'size': 24, 'dpi': 96},
-                {'filename': self.tempdir + "/" + "image_032.png", 'size': 32, 'dpi': 96},
-                {'filename': self.tempdir + "/" + "image_048.png", 'size': 48, 'dpi': 96},
-                {'filename': self.tempdir + "/" + "image_064.png", 'size': 64, 'dpi': 96},
-                {'filename': self.tempdir + "/" + "image_072.png", 'size': 72, 'dpi': 96},
-                {'filename': self.tempdir + "/" + "image_096.png", 'size': 96, 'dpi': 96},
-                {'filename': self.tempdir + "/" + "image_128.png", 'size': 128, 'dpi': 96},
-                {'filename': self.tempdir + "/" + "image_180.png", 'size': 180, 'dpi': 96},
-                {'filename': self.tempdir + "/" + "image_256.png", 'size': 256, 'dpi': 96},
+                {'filename': basename + "-16.png", 'size': 16, 'dpi': 96, 'ico': True},
+                {'filename': basename + "-24.png", 'size': 24, 'dpi': 96, 'ico': True},
+                {'filename': basename + "-32.png", 'size': 32, 'dpi': 96, 'ico': True},
+                {'filename': basename + "-48.png", 'size': 48, 'dpi': 96, 'ico': True},
+                {'filename': basename + "-64.png", 'size': 64, 'dpi': 96, 'ico': True},
+                {'filename': basename + "-128.png", 'size': 128, 'dpi': 96, 'ico': True},
+                {'filename': basename + "-256.png", 'size': 256, 'dpi': 96, 'ico': True},
+                {'filename': basename + "-512.png", 'size': 512, 'dpi': 96, 'ico': False},
+                {'filename': basename + "-1024.png", 'size': 1024, 'dpi': 96, 'ico': False},
+                # iOS icons
+                {'filename': basename + "-App-20x20@1x.png", 'size': 20, 'dpi': 72, 'ico': False},
+                {'filename': basename + "-App-20x20@2x.png", 'size': 40, 'dpi': 144, 'ico': False},
+                {'filename': basename + "-App-20x20@3x.png", 'size': 60, 'dpi': 216, 'ico': False},
+                {'filename': basename + "-App-29x29@1x.png", 'size': 29, 'dpi': 72, 'ico': False},
+                {'filename': basename + "-App-29x29@2x.png", 'size': 58, 'dpi': 144, 'ico': False},
+                {'filename': basename + "-App-29x29@3x.png", 'size': 87, 'dpi': 216, 'ico': False},
+                {'filename': basename + "-App-40x40@1x.png", 'size': 40, 'dpi': 72, 'ico': False},
+                {'filename': basename + "-App-40x40@2x.png", 'size': 80, 'dpi': 144, 'ico': False},
+                {'filename': basename + "-App-40x40@3x.png", 'size': 120, 'dpi': 216, 'ico': False},
+                {'filename': basename + "-App-60x60@2x.png", 'size': 120, 'dpi': 144, 'ico': False},
+                {'filename': basename + "-App-60x60@3x.png", 'size': 180, 'dpi': 216, 'ico': False},
+                {'filename': basename + "-App-76x76@1x.png", 'size': 76, 'dpi': 72, 'ico': False},
+                {'filename': basename + "-App-76x76@2x.png", 'size': 152, 'dpi': 144, 'ico': False},
+                {'filename': basename + "-App-83.5x83.5@2x.png", 'size': 167, 'dpi': 144, 'ico': False},
+                {'filename': basename + "-App-1024x1024@1x.png", 'size': 1024, 'dpi': 72, 'ico': False},
             ]
 
             for exportfilepar in export:
                 inkscape(self.options.input_file, export_dpi=exportfilepar['dpi'], export_filename=exportfilepar['filename'], export_width=exportfilepar['size'], export_height=exportfilepar['size'])
 
-            self.createIconFile(export)
+            self.createIconFile(export, basename + ".ico")
 
             # inkex.utils.debug("finished")
         except Exception as e:
-            inkex.errormsg("Exception! %s " % e.message)
+            inkex.errormsg("Exception occured!")
 
 effect = CreateIconFile()
 effect.run()
--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # inkscape-createiconfile
-An extension for Inkscape that export a drawing to a icon file
+An extension for Inkscape that export a drawing to a icon file,
+and create .png files in various siyes.
 
 ## How to install
 Copy the extension directory in Inkscape extensions directory.
@@ -8,11 +9,14 @@ Inkscape extensions directory can be found with Edit > Preferences > System > Us
 
 ## Use
 
-1. Create a square drawing sheet e.g. 256 x 256 pixels
+1. Create a square drawing sheet e.g. 1024 x 1024 pixels
 2. Draw any elements
 3. Go to Extensions -> Export -> CreateIconFile...
-4. Select a Folder and a filename and press "Apply"
+4. Select an icon filename and press "Apply"
+5. The extension creates an .ico file and a number of .png files in the same folder as the
+   .svg file which you're exporting. These .png files are useful for Windows, macOS, and iOS deployment.
 
 ## About
 
 Author: Sven Schork
+Updates by: Florian Bomers
